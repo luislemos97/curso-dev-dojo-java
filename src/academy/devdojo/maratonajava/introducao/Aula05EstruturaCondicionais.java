9633df960d53b1cfde58d545c04ab74e8c3057b1
luis lemos <luislemosm12@gmail.com>
2024-01-02 18:30:49 -0300

    subindo a aula 05 com fundamentos das estruturas condicionais.

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturaCondicionais.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturaCondicionais {
+    public static void main(String[] args) {
+        int idade = 20;
+        boolean IsAutorizadoComprarBebida = idade >= 18;
+        if (IsAutorizadoComprarBebida) {
+            System.out.println("Autorizado a comprar bebidas alcoólicas.");
+        }
+
+        if (IsAutorizadoComprarBebida == false){
+            System.out.println("Não autorizado a comprar bebidas alcoólicas.");
+        }
+
+        int salario = 3500;
+        boolean IsAutorizadoComprarIphone = salario >= 4500;
+        if (IsAutorizadoComprarIphone) {
+            System.out.println("Autorizado a comprar um Iphone.");
+        }
+        if (!IsAutorizadoComprarIphone)
+            System.out.println("Não Autorizado a comprar um Iphone.");
+    }
+}
