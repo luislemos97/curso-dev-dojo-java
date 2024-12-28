46471d32295d464df2bda33d0134d954b76415e8
Luis Lemos <luislemosm12@gmail.com>
2024-12-28 17:02:53 -0300

    adcionando algumas classes no mudolo introdução

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -4,8 +4,8 @@ public class Aula05EstruturasCondicionais03 {
     public static void main(String[] args) {
         // Doar se salario > 5000
         double salario = 3000;
-        String mensagemDoar = "Eu vou doar 5000 por DevDojo";
-        String mensagemNaoDoar = "Eu não vou doar 5000 por DevDojo";
+        String mensagemDoar = "Eu vou doar 5000 para o DevDojo";
+        String mensagemNaoDoar = "Eu não vou doar 5000 para o DevDojo";
         //(Condição) ? verdadeiro : falso
         String resultado = salario > 5000 ? mensagemDoar :mensagemNaoDoar;
 
