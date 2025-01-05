7a588d2af7f09ca6638a6d19b6ef8823353b9fea
Luis Lemos <luislemosm12@gmail.com>
2025-01-05 20:01:14 -0300

    feat: iteração por foreach

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
@@ -1,19 +1,14 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula07Arrays03 {
+        
     public static void main(String[] args) {
-        int [][] dias = new int [3] [3];
-        dias [0][0] = 31;
-        dias [0][1] = 28;
-        dias [0][2] = 31;
+        int [] numeros = new int[3];
+        int [] numeros2 = {1,2,3,4,5};
+        int [] numeros3 = new int[]{1,2,3,4,5};
 
-        dias [1][0] = 31;
-        dias [1][1] = 28;
-        dias [1][2] = 28;
-
-        for (int i = 0; i < dias.length; i++) {
-            for (int j = 0; j < dias[i].length; j++) 
-            System.out.println(dias[i][j]);
+        for (int num : numeros3) {
+            System.out.println(num);
         }
     }
 }
