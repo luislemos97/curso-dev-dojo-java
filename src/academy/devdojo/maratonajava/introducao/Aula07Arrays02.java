cca84fde62c168e20366f5037b0e1cf9581f40c4
Luis Lemos <luislemosm12@gmail.com>
2025-01-05 18:38:45 -0300

    Update: anotações e implementação em nova classe

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -8,6 +8,8 @@ public class Aula07Arrays02 {
        nomes [1] = "Goku";
        nomes [2] = "Vegeta";
 
+       // Toda vez que troca o indice preciso mudar em tudo, senão dar um exception.
+       //quando eu coloco length é para retornar o tamanho do array, tendo isso não preciso alterar no for.
        for (int i = 0; i < nomes.length; i++) {
         System.out.println(nomes[i]);
        }
